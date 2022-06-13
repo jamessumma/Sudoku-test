6f2eba22be31d4087d7d436e9fd9e6f929543b7e
james <jsumma3167@westfield.ma.edu>
2022-06-13 12:42:02 -0400

    Game Logic

--- src/main/java/sudoku/SudokuApplication.java
+++ src/main/java/sudoku/SudokuApplication.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sudoku.userinterface.IUserInterfaceContract;
 import sudoku.userinterface.UserInterfaceImpl;
 
 import java.io.IOException;
